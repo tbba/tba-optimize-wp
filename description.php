7a84341420fbf7b9c7f8d766756835e7f1eef995
Carl <lists@tba-berlin.de>
2024-10-03 23:06:28 +0200

    Update description.php

--- description.php
+++ description.php
@@ -4,14 +4,23 @@
 <div class="tba-description">
     <h2>TBA Optimization for Speed and GDPR</h2>
     <p>
-      
-      This function optimizes your WordPress site for both speed 
+      This Plugin optimizes your WordPress site for both speed 
       and GDPR compliance by removing unnecessary features while 
       ensuring that Web-Application Firewalls (like NinjaFirewall) can still handle XML-RPC and REST API
-      security features as intended. It disables emojis, embeds, REST API links, 
+      security features as intended. 
+    </p>
+<p>        
+    It disables emojis, embeds, REST API links, 
       and Dashicons for non-logged-in users. Additionally, it improves performance by 
       removing jQuery Migrate for guests and optionally removes HTML comments for 
-      better front-end optimization. Each feature can be toggled individually 
+      better front-end optimization. 
+    </p>
+<p>  
+    Each feature can be toggled individually 
       to suit your site's needs, ensuring compliance with GDPR and faster load times for users.</p>
+
+    <p>
+       <a href="https://github.com/tbba/tba-optimize-wp" target="_blank"> Link zum Quelltext auf GitHub</a>
+</p>  
 </div>
 
