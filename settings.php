36ba574f701deb64194a70eab4945240796fb324
Carl <lists@tba-berlin.de>
2024-10-03 19:24:27 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,10 +3,15 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-// Default setting for enabling optimizations
+// Default settings for the plugin
 function tba_optimize_default_options() {
     return array(
-        'enable_optimizations' => true,
+        'disable_emojis' => true,
+        'remove_embeds' => true,
+        'disable_rest_api' => true,
+        'disable_dashicons' => true,
+        'remove_jquery_migrate' => true,
+        'remove_html_comments' => true
     );
 }
 
@@ -40,10 +45,50 @@ function tba_optimize_render_settings_page() {
             ?>
             <table class="form-table">
                 <tr valign="top">
-                    <th scope="row">Enable Optimizations</th>
+                    <th scope="row">Disable Emojis</th>
                     <td>
-                        <input type="checkbox" name="tba_optimize_options[enable_optimizations]" value="1" <?php checked(1, isset($options['enable_optimizations']) ? $options['enable_optimizations'] : 0); ?> />
-                        <label for="tba_optimize_options[enable_optimizations]">Enable all optimization options (default: on)</label>
+                        <input type="checkbox" name="tba_optimize_options[disable_emojis]" value="1" <?php checked(1, isset($options['disable_emojis']) ? $options['disable_emojis'] : 0); ?> />
+                        <label for="tba_optimize_options[disable_emojis]">Disable Emojis</label>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Remove Embeds</th>
+                    <td>
+                        <input type="checkbox" name="tba_optimize_options[remove_embeds]" value="1" <?php checked(1, isset($options['remove_embeds']) ? $options['remove_embeds'] : 0); ?> />
+                        <label for="tba_optimize_options[remove_embeds]">Remove Embeds</label>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Disable REST API Links</th>
+                    <td>
+                        <input type="checkbox" name="tba_optimize_options[disable_rest_api]" value="1" <?php checked(1, isset($options['disable_rest_api']) ? $options['disable_rest_api'] : 0); ?> />
+                        <label for="tba_optimize_options[disable_rest_api]">Disable REST API Links and Headers</label>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Disable Dashicons for Non-Logged-In Users</th>
+                    <td>
+                        <input type="checkbox" name="tba_optimize_options[disable_dashicons]" value="1" <?php checked(1, isset($options['disable_dashicons']) ? $options['disable_dashicons'] : 0); ?> />
+                        <label for="tba_optimize_options[disable_dashicons]">Disable Dashicons for Non-Logged-In Users</label>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Remove jQuery Migrate</th>
+                    <td>
+                        <input type="checkbox" name="tba_optimize_options[remove_jquery_migrate]" value="1" <?php checked(1, isset($options['remove_jquery_migrate']) ? $options['remove_jquery_migrate'] : 0); ?> />
+                        <label for="tba_optimize_options[remove_jquery_migrate]">Remove jQuery Migrate</label>
+                    </td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row">Remove HTML Comments</th>
+                    <td>
+                        <input type="checkbox" name="tba_optimize_options[remove_html_comments]" value="1" <?php checked(1, isset($options['remove_html_comments']) ? $options['remove_html_comments'] : 0); ?> />
+                        <label for="tba_optimize_options[remove_html_comments]">Remove HTML Comments</label>
                     </td>
                 </tr>
             </table>
@@ -53,16 +98,13 @@ function tba_optimize_render_settings_page() {
     <?php
 }
 
-// Validate options
+// Validate and sanitize options
 function tba_optimize_validate_options($input) {
-    $input['enable_optimizations'] = isset($input['enable_optimizations']) ? 1 : 0;
+    $input['disable_emojis'] = isset($input['disable_emojis']) ? 1 : 0;
+    $input['remove_embeds'] = isset($input['remove_embeds']) ? 1 : 0;
+    $input['disable_rest_api'] = isset($input['disable_rest_api']) ? 1 : 0;
+    $input['disable_dashicons'] = isset($input['disable_dashicons']) ? 1 : 0;
+    $input['remove_jquery_migrate'] = isset($input['remove_jquery_migrate']) ? 1 : 0;
+    $input['remove_html_comments'] = isset($input['remove_html_comments']) ? 1 : 0;
     return $input;
 }
-
-// Add a settings link in the plugin list
-function tba_optimize_plugin_settings_link($links) {
-    $settings_link = '<a href="options-general.php?page=tba-optimize-settings">Settings</a>';
-    array_push($links, $settings_link);
-    return $links;
-}
-add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'tba_optimize_plugin_settings_link');
